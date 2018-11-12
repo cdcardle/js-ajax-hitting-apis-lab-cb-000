e09e7dbbed89c23d976cbe236109359bbfc8c679
Chris Cardle <cdcardle@gmail.com>
2018-11-12 18:22:20 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 function getRepositories() {
   let username = document.getElementById('username').value;
   const req = new XMLHttpRequest();
-  req.addEventListener('load', showRepositories);
+  req.addEventListener('load', displayRepositories);
   req.open('GET', 'https://api.github.com/users/' + `${username}` '/repos');
   req.send();
 }
 
-function showRepositories() {
+function displayRepositories() {
 
 }
