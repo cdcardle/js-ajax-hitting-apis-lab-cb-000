ecdaccc7819ef73bf34a5be4b0751e119cebaf9c
Chris Cardle <cdcardle@gmail.com>
2018-11-12 18:37:49 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -1,14 +1,11 @@
-// function displayCommits() {
-//   const commits = JSON.parse(this.responseText);
-//
-// }
+let gitHubUsers = 'https://api.github.com/users';
+let username = document.getElementById('username').value;
 
 function getRepositories() {
-  let username = document.getElementById('username').value;
   if (username) {
     const req = new XMLHttpRequest();
     req.addEventListener('load', displayRepositories);
-    req.open('GET', 'https://api.github.com/users/' + `${username}` + '/repos');
+    req.open('GET', `${gitHubUsers}/${username}` + '/repos');
     req.send();
   }
 }
@@ -18,4 +15,17 @@ function displayRepositories() {
   console.log(repos);
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`;
   document.getElementById('repositories').innerHTML = repoList;
+}
+
+function getCommits(el) {
+  const name = el.dataset.repo;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showCommits);
+  req.open('GET', 'https://api.github.com/repos/' + `${username}` + '/' + name + '/commits');
+  req.send();
+}
+
+function displayCommits() {
+  const commits = JSON.parse(this.responseText);
+  
 }
