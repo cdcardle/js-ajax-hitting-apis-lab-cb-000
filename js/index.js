2fc16d2db022c10be59ac317d9b121c870d80356
Chris Cardle <cdcardle@gmail.com>
2018-11-12 18:31:48 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -15,4 +15,5 @@ function displayRepositories() {
   let repos = JSON.parse(this.responseText);
   console.log(repos);
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`;
+  document.getElementById('repositories').innerHTML = repoList;
 }
