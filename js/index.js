1c98025b8e358354e3b7fd7f30483a9c36186caf
Chris Cardle <cdcardle@gmail.com>
2018-11-12 18:27:14 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 // function displayCommits() {
 //   const commits = JSON.parse(this.responseText);
-// 
+//
 // }
 
 function getRepositories() {
   let username = document.getElementById('username').value;
   const req = new XMLHttpRequest();
   req.addEventListener('load', displayRepositories);
-  req.open('GET', 'https://api.github.com/users/' + `${username}` '/repos');
+  req.open('GET', 'https://api.github.com/users/' + `${username}` + '/repos');
   req.send();
 }
 
